d99036992943889613dad4745ef315473713d0c4
Matthew Soars <matthewsoars@hotmail.co.uk>
2024-06-22 21:54:23 +0100

    Main Guincorn Changes

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 import uvicorn
-from app import app
 
 if __name__ == "__main__":
     uvicorn.run(
-        app,
+        "app:app",
         host="0.0.0.0",
         port=8080,
-        log_level="debug",  # Set log level to 'info' or 'debug' for more verbosity
-        reload=True,       # Enables auto-reload in development mode
+        log_level="info",
+        reload=True  # Enables auto-reload in development mode
     )
