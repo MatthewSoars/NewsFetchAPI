0998b05cd904b9dec38eff4746fec51e7920da89
Matthew Soars <matthewsoars@hotmail.co.uk>
2024-07-10 00:02:36 +0100

    Updating GEO part 2

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ if __name__ == "__main__":
     uvicorn.run(
         "app:app",
         host="0.0.0.0",
-        port=8000,
+        port=6000,
         log_level="info",
         reload=True  # Enables auto-reload in development mode
     )
